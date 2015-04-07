e96a2d5076cac1ecf014691edb2ebc00c0965917
Balaji <Balaji.Varanasi@gmail.com>
2015-04-07 17:57:29 -0600

    Updated Test
    Updates test to be more platform independent

--- chapter4/final/gswm/src/test/java/HelloWorldTest.java
+++ chapter4/final/gswm/src/test/java/HelloWorldTest.java
@@ -5,27 +5,37 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import java.io.IOException;
 
 
+/**
+   Updated the test so that it is more platform independent
+*/
 public class HelloWorldTest {
 
 	private final ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-	
+
 	@Before
 	public void setUp() {
-	    System.setOut(new PrintStream(outStream));
+		System.setOut(new PrintStream(outStream));
 	}
 
 	@Test
-	public void testSayHello() {
+	public void testSayHello2() throws IOException {
 		HelloWorld hw = new HelloWorld();
 		hw.sayHello();
-		Assert.assertEquals("Hello World", outStream.toString());
+
+		outStream.flush();
+        String printedOutPut = new String(outStream.toByteArray());
+		String[] outputData = printedOutPut.split(System.getProperty("line.separator"));
+
+
+		Assert.assertEquals("Hello World", outputData[0]);
 	}
-	
+
 	@After
-	public void cleanUp() {
-	    System.setOut(null);
+	public void cleanUp(){
+		System.setOut(null);
 	}
 	
 }
